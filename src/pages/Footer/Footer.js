5cc321e32d482c640d31a7b48d9e053bc8dce4b6
firoj7github <fkfiroj02@gmail.com>
2022-06-08 20:39:08 -0700

    ggg

--- src/pages/Footer/Footer.js
+++ src/pages/Footer/Footer.js
@@ -4,7 +4,16 @@ import './Footer.css'
 const Footer = () => {
     return (
         <div className='footer-all'>
-            
+            <h3>Hire me any kind of website design and website development </h3>
+            <p>If you have any sample design, just let me know. If you do <span className='sample'>not, just relax and let me awe you with my work.</span> </p>
+           
+             
+            <form>
+                <input type='email'name='email' ></input>
+                <input value='submit' >submit</input>
+
+            </form>
+              
         </div>
     );
 };
