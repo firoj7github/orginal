072063c36ed586acc9c2f48d4c848f58aa1872bc
firoj7github <fkfiroj02@gmail.com>
2022-06-28 22:39:12 -0700

    navbar

--- src/pages/Home/Protfolio/Protfolio.js
+++ src/pages/Home/Protfolio/Protfolio.js
@@ -100,48 +100,35 @@ const Protfolio = () => {
       }}
     />
    </div>
-   {/* <nav class="navbar focus navbar-expand-lg navbar-white bg-dark text-white">
+   <nav class="navbar navbar-expand-lg navbar-light ">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <a class="navbar-brand text-white" href="#"><h3>Protfo<span className='span-title'>lio.</span></h3></a>
+    <button class="navbar-toggler bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active text-white" aria-current="page" href="#">Home</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+        <li class="nav-item ">
+          <a class="nav-link text-white" href="#">About</a>
         </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-           
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
+        <li class="nav-item ">
+          <a class="nav-link text-white" href="#">Project</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        <li class="nav-item ">
+          <a class="nav-link text-white" href="#">Contact</a>
         </li>
+      
+       
       </ul>
       
     </div>
   </div>
-</nav> */}
-    <div className='header-all'>
-        <h3>Protfo<span className='span-title'>lio.</span></h3>
-        <ul>
-            <Link to='/'>Home</Link>
-            <Link to='about'>About</Link>
-            <Link to='/blog'>Blog</Link>
-            <Link to=''>Contact</Link>
-        </ul>
-    </div>
+</nav>
+   
+    
     
 
     <div className='head-manage'>
